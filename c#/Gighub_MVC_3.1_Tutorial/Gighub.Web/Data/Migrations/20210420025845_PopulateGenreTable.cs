581a66fa3872d7af46b6b9e22c8239763b75758d
Matt <Matt@WAFFLES>
2021-04-19 23:20:11 -0400

    populated genre database

---
+++ c#/Gighub_MVC_3.1_Tutorial/Gighub.Web/Data/Migrations/20210420025845_PopulateGenreTable.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Gighub.Web.Data.Migrations
+{
+    public partial class PopulateGenreTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData("Genres", "Name", "Jazz");
+            migrationBuilder.InsertData("Genres", "Name", "Blues");
+            migrationBuilder.InsertData("Genres", "Name", "R&B");
+            migrationBuilder.InsertData("Genres", "Name", "Rock");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
