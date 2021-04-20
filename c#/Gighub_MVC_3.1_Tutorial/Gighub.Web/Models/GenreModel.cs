8d7a78b3b659d6ac506280e2aeee9014caff0ceb
Matt <Matt@WAFFLES>
2021-04-19 22:08:35 -0400

    Setup gig and genre model

---
+++ c#/Gighub_MVC_3.1_Tutorial/Gighub.Web/Models/GenreModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Gighub.Web.Models
+{
+    public class GenreModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ c#/Gighub_MVC_3.1_Tutorial/Gighub.Web/Models/GigModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Gighub.Web.Models
+{
+    public class GigModel
+    {
+        public int Id { get; set; }
+        public string ArtisitId { get; set; }
+        public DateTime DateTime { get; set; }
+        public string Venue { get; set; }
+        public GenreModel Genre { get; set; }
+    }
+}
